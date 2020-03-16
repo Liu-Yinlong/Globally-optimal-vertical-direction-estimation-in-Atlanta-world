d42fcaf246851a906f38fee6f2ea5e707f44e3aa
Liu-Yinlong <50229242+Liu-Yinlong@users.noreply.github.com>
2020-03-16 21:51:49 +0100

    Update Angle_point_side_elevation.m

--- Globally-Searching-Spherical/Angle_point_side_elevation.m
+++ Globally-Searching-Spherical/Angle_point_side_elevation.m
@@ -5,11 +5,23 @@ function [angle_min,angle_max] = Angle_point_side_elevation(data,a,b1,b2)
     angle_min=zeros(1,NUM);
     angle_max=zeros(1,NUM);
     
-    x=data(1,:);
-    y=data(2,:);
-    ind_in=(y<=b2 &y>=b1);
+%     x=data(1,:);
+%     y=data(2,:);
     
-    t=acos(cos(a-x(ind_in))).*y(ind_in);
+    
+    
+%     ind_in=(y<=b2 &y>=b1);
+%     t=acos(cos(a-x(ind_in))).*y(ind_in);
+     
+    h2=atan2(data(3,:),data(1,:)*cos(a)+data(2,:)*sin(a));
+    
+     ind_in=(h2<=b2 &h2>=b1);
+    
+   
+    vertex_2d=[repmat(a,1,size(h2(ind_in),2));h2(ind_in)];
+    vertex_3d= scsMapping_point_2d_3d(vertex_2d);
+    
+    t=acos(dot(data(:,ind_in),vertex_3d));
     angle_min(ind_in)=t;
     angle_min(~ind_in)=nan;
         
